278807266d1affe6859f17f88c051581b303e2ac
pcarff <pcarff@gmail.com>
2017-06-03 15:33:52 -0400

    Changed to using PhProbe class. Implemented readSample to return pH (float).

--- app/src/main/java/com/anzym/testi2c/MainActivity.java
+++ app/src/main/java/com/anzym/testi2c/MainActivity.java
@@ -17,35 +17,9 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
+
     PhProbe mPhProbe;
     private static final String I2C_DEVICE_NAME = "I2C1";   // BUS NAME
-
-    /** <TODO> Remove this after suscessful conversion to using driver.
-
-    private static final int PH_PROBE_ADDRESS = 99;  //0x63
-    private static final int TDS_PROBE_ADDRESS = 100;  //0x64
-    private static final int BUFFER_SIZE = 20;
-
-    private int delay_time = 300;
-
-    // <TODO> Update these to strings and the use getBytes
-    //ASCII for i = 105
-    private static final String PH_INFO = "i";
-    //ASCII for r = 114
-    private static final String PH_READ = "r";
-
-
-    private I2cDevice pHProbe;
-    private I2cDevice tDSProbe;
-
-    private char[] cmd;
-    private byte[] cmdBytes;
-    private byte[] readBytes;
-    private byte[] buffer;
-    private int code;
-    private char[] phData;
-     </TODO>
-     */
 
     private float pH;
 
@@ -61,118 +35,27 @@ public class MainActivity extends AppCompatActivity {
             //couldn't configure the device
         }
 
+
         // Read ph value
-
         try {
-            sample = PhProbe.r
-        }
-
-
-
-        /*
-        // Initialize variables.
-        cmd = new char[BUFFER_SIZE];
-        cmdBytes = new byte[BUFFER_SIZE];
-        readBytes = new byte[BUFFER_SIZE];
-        buffer = new byte[BUFFER_SIZE];
-        phData = new char[BUFFER_SIZE];
-
-        // Set up I2C bus and devices
-        try {  //TODO consider trying each device separately to get error for that device
-            PeripheralManagerService service = new PeripheralManagerService();
-            pHProbe = service.openI2cDevice(I2C_DEVICE_NAME, PH_PROBE_ADDRESS);
-            //tDSProbe = service.openI2cDevice(I2C_DEVICE_NAME, TDS_PROBE_ADDRESS);
+            pH = mPhProbe.readSample();
+            Log.d(TAG,"ph: " + pH);
         } catch (IOException e) {
-            Log.w(TAG, "Unable to access I2C device", e);
-        }
-
-
-
-        // convert char[] to byte[]
-        //<TODO> remove the new String after I make these Strings above.
-        //cmdBytes =  PH_READ.getBytes();
-        cmdBytes = PH_INFO.getBytes();
-
-        // <TODO> move this for method when executing commands.
-        if (cmdBytes[0] == 'c' || cmdBytes[0] == 'r') {
-            delay_time = 1800;
-        } else {
-            delay_time = 300;
-        }
-
-        try {
-            pHProbe.write(cmdBytes, cmdBytes.length);
-            Log.d(TAG,"Wrote: " + new String(cmdBytes));
-        } catch (IOException e) {
-            Log.d(TAG, "Error writing to device: " + e);
-        }
-
-        // Wait time for circuit to complete intstruction
-        try {
-            Log.d(TAG, "Waiting : " + delay_time + " secs.");
-            Thread.sleep(delay_time);
-        } catch (InterruptedException e) {
-            Log.d(TAG, "delay time error: " + e);
-        }
-
-        //read result code back from circuit
-        try {
-            // read all bytes
-            //pHProbe.read(readBytes, BUFFER_SIZE);
-
-            // read one byte to get code
-            pHProbe.read(readBytes, BUFFER_SIZE);
-            code = readBytes[0];
-            Log.d(TAG, "code : " + code);
-            Log.d(TAG, "Read results: " + new String(readBytes, "UTF-8"));
-        } catch (IOException e) {
-            Log.d(TAG, "Error reading data: " + e);
-        }
-
-        switch (code) {
-            case 1:
-                Log.d (TAG, "Read was success");
-                break;
-            case 2:
-                Log.d (TAG, "Read Failure");
-                break;
-            case 254:
-                Log.d (TAG, "Read Pending");   //Command has not yet finished executing
-                break;
-            case 255:
-                Log.d (TAG, "Read no data");
-                break;
 
         }
-
-        //<TODO> Do we need to CLOSE the device??
-
-        */
-
     }
 
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if (pHProbe != null) {
+        if (mPhProbe != null) {
             try {
-                pHProbe.close();
-                pHProbe = null;
+                mPhProbe.close();
+                mPhProbe = null;
             } catch (IOException e) {
-                Log.w(TAG, "Unable to close I2C device " + pHProbe.toString(), e);
-            }
-        }
-        if (tDSProbe != null) {
-            try {
-                tDSProbe.close();
-                tDSProbe = null;
-            } catch (IOException e) {
-                Log.w(TAG, "Unable to close I2C device" + tDSProbe.toString(), e);
+                Log.w(TAG, "Unable to close I2C device " + mPhProbe.toString(), e);
             }
         }
     }
-
-
-
 }
--- app/src/main/java/com/anzym/testi2c/driver/PhProbe.java
+++ app/src/main/java/com/anzym/testi2c/driver/PhProbe.java
@@ -1,10 +1,14 @@
 package com.anzym.testi2c.driver;
 
 
+import android.util.Log;
+
 import com.google.android.things.pio.I2cDevice;
 import com.google.android.things.pio.PeripheralManagerService;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
 
 /**
  * Created by pcarff on 6/2/17.
@@ -16,10 +20,15 @@ public class PhProbe  implements AutoCloseable {
 
     private static final String TAG = PhProbe.class.getSimpleName();
 
+    private int delay_time = 300;
     /**
      * I2C slave address for ph circuit
      */
-    public static final int I2C_ADDRESS = 0x63;
+    private static final int I2C_ADDRESS = 0x63;
+    private static final int BUFFER_SIZE = 20;
+    private static final String READ_PH = "r";
+    private static final String GET_INFO = "i";
+
 
 
     private I2cDevice mDevice;
@@ -68,10 +77,78 @@ public class PhProbe  implements AutoCloseable {
      *
      */
     public float readSample() throws IOException, IllegalStateException {
+
+        float pH = 0;
+        int resultCode;
+        byte[] readResult = new byte[BUFFER_SIZE];
+        readResult = getResult(READ_PH);
+        Log.d(TAG,"readResult: " + new String(readResult, "UTF-8"));
+        resultCode = (int)readResult[0];
+        switch (resultCode) {
+            case 1:
+                Log.d(TAG, "Read was success");
+                int counter = 0;
+                for (int i =1; i < readResult.length; i++) {
+                    if (readResult[i] != 0) {
+                        Log.d(TAG, "readResult[" + i + "]: " + readResult[i]);
+                        counter = i;
+                    }
+                }
+                byte[] pHbByte = Arrays.copyOfRange(readResult, 1, counter+1);
+                Log.d(TAG,"phByte: " + new String(pHbByte));
+
+                pH = Float.valueOf(new String(pHbByte));
+                break;
+            case 2:
+                Log.d (TAG, "Read Falure");
+                break;
+            case 254:
+                Log.d (TAG, "Read Pending");   //Command has not yet finished executing
+                break;
+            case 255:
+                Log.d (TAG, "Read no data");
+                break;
+        }
+
+        return pH;
+
+    }
+
+    private byte[] getResult(String cmdStr) {
+        byte[] readBytes = new byte[BUFFER_SIZE];
+        byte code;
         if (mDevice == null ) {
             throw new IllegalStateException("device not connected");
         }
-        
+        byte[] cmdBytes = cmdStr.getBytes();
+        if (cmdBytes[0] == 'c' || cmdBytes[0] == 'r') {
+            delay_time = 1800;
+        } else {
+            delay_time = 500;
+        }
+        try {
+            mDevice.write(cmdBytes, cmdBytes.length);
+        } catch (IOException e) {
+            Log.d(TAG, "Error writing to device: " + e);
+        }
+        // Wait time for circuit to complete intstruction
+        try {
+            Thread.sleep(delay_time);
+        } catch (InterruptedException e) {
+            Log.d(TAG, "delay time error: " + e);
+        }
+
+        //read result code back from circuit
+        try {
+            // read all bytes
+            mDevice.read(readBytes, BUFFER_SIZE);
+            code = readBytes[0];
+
+
+        } catch (IOException e) {
+            Log.d(TAG, "Error reading data: " + e);
+        }
+        return readBytes;
     }
 
     /**
